b4193de43a3949bb71a00017df6fa9656b19d4f2
LuChangDie <1403105250@qq.com>
2017-04-07 09:27:00 +0800

    定义GSON实体类

---
+++ app/src/main/java/android/lcd/coolweather/gson/AQI.java
@@ -0,0 +1,16 @@
+package android.lcd.coolweather.gson;
+
+/**
+ * Created by Administrator on 2017/4/6.
+ */
+
+public class AQI {
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
