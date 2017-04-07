1fc1edd459c0a2783cc88d65a051bec83a9c8303
LuChangDie <1403105250@qq.com>
2017-04-07 11:07:12 +0800

    将天气显示到界面上

--- app/src/main/java/android/lcd/coolweather/fragment/ChooseAreaFragment.java
+++ app/src/main/java/android/lcd/coolweather/fragment/ChooseAreaFragment.java
@@ -3,7 +3,9 @@ package android.lcd.coolweather.fragment;
 
 import android.app.Fragment;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.lcd.coolweather.R;
+import android.lcd.coolweather.activity.WeatherActivity;
 import android.lcd.coolweather.db.City;
 import android.lcd.coolweather.db.County;
 import android.lcd.coolweather.db.Province;
@@ -89,6 +91,12 @@ public class ChooseAreaFragment extends Fragment {
                 } else if (currentLevel == LEVEL_CITY){
                     selectedCity = cityList.get(position);
                     queryCounties();
+                } else if (currentLevel == LEVEL_COUNTY) {
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                    intent.putExtra("weather_id",weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
--- app/src/main/java/android/lcd/coolweather/util/Utility.java
+++ app/src/main/java/android/lcd/coolweather/util/Utility.java
@@ -3,7 +3,10 @@ package android.lcd.coolweather.util;
 import android.lcd.coolweather.db.City;
 import android.lcd.coolweather.db.County;
 import android.lcd.coolweather.db.Province;
+import android.lcd.coolweather.gson.Weather;
 import android.text.TextUtils;
+
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -71,5 +74,18 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    //将返回的JSON数据解析成Weather实体类
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
